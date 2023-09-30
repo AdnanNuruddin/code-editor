d5d99dd6abb5d7dde120f1fc96b4b14b4e17281c
Adnan Nuruddin <get2adnan@windowslive.com>
2023-09-30 13:59:23 +0600

    Data submission added

--- App.tsx
+++ App.tsx
@@ -1,26 +1,78 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import * as React from 'react';
+import { View, Text, Modal, Pressable, StyleSheet } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useKeyboard } from '@react-native-community/hooks';
 import CodeEditor, { CodeEditorSyntaxStyles } from '@rivascva/react-native-code-editor';
+import { TouchableOpacity } from 'react-native';
 
-const Example = (): JSX.Element => {
+const Editor = (): JSX.Element => {
+
+  const [code, setCode] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [responseData, setResponseData] = useState<{ error: string, output: string }>({ error: '', output: '' });
+
+  const submitCode = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('http://code.jomakhata.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code }),
+      });
+      setResponseData(await response.json());
+      setModalVisible(true);
+      console.log(responseData);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <SafeAreaView>
       <CodeEditor
         style={{
           ...{
-            fontSize: 16,
-            inputLineHeight: 16,
-            highlighterLineHeight: 16,
+            fontSize: 18,
+            inputLineHeight: 18,
+            highlighterLineHeight: 18,
+            height: '95%',
           },
         }}
-        language="cpp"
+        language={'cpp'}
+        onChange={(data) => { setCode(data); }}
         syntaxStyle={CodeEditorSyntaxStyles.atomOneDark}
         showLineNumbers
       />
+      <TouchableOpacity style={{ height: '5%' }} disabled={loading} onPress={() => { submitCode() }}>
+        <View style={{ backgroundColor: 'blue', padding: 10, display: 'flex', alignItems: 'center' }}>
+          <Text style={{ color: 'white' }}>{loading ? 'LOADING...' : 'COMPILE & RUN'}</Text>
+        </View>
+      </TouchableOpacity>
+      <Modal
+        animationType={'fade'}
+        transparent={true}
+        visible={modalVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={{ fontSize: 18, fontFamily: 'consolas' }}>Output</Text>
+            <Text style={{ fontSize: 45, fontFamily: 'consolas' }}>------------------------------------------</Text>
+            <Text style={{ fontSize: 16, fontFamily: 'consolas' }}>{responseData.output}</Text>
+            <Text style={{ color: 'red', fontSize: 16, fontFamily: 'consolas' }}>{responseData.error}</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Done</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -28,10 +80,52 @@ const Example = (): JSX.Element => {
 function App(): JSX.Element {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Example />
+      <Editor />
     </SafeAreaView>
   );
-
 }
-
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    paddingHorizontal: 25,
+    backgroundColor: 'blue',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+});
 export default App;
